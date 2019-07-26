0d0e96e30ea735d1c144c7f01215fa293b766bc4
System Administrator <root@Maxs-MacBook-Pro.local>
2019-07-26 15:17:48 -0400

    Spring Security BCrypt Password Encoder

--- src/main/java/com/telusko/secureapp/AppSecurityConfig.java
+++ src/main/java/com/telusko/secureapp/AppSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -34,7 +35,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         logger.debug("userDetailsService: " + this.userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
     /*
